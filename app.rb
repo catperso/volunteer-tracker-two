eb95f3908e98156826a376e630243ee1ca2c6d61
Matt Casperson <mc.casperson@gmail.com>
2021-12-26 21:46:37 -0800

    add layout for projects page and add routing to clear all the databases

--- app.rb
+++ app.rb
@@ -2,12 +2,24 @@ require 'sinatra'
 require 'sinatra/reloader'
 also_reload 'lib/**/*.rb'
 require 'pry'
-require "pg"
+require 'pg'
 require './lib/project'
 require './lib/volunteer'
 
 DB = PG.connect({ dbname: 'volunteer_tracker', host: 'db', user: 'postgres', password: 'password' })
 
-get '/' do
-  "This is connected to the database #{DB.db}."
+get('/') do
+  @projects = Project.all
+  redirect to('/projects')
+end
+
+get('/projects') do
+  @projects = Project.all
+  erb(:projects)
+end
+
+delete('/projects') do
+  Project.clear
+  Volunteer.clear
+  redirect to('/projects')
 end
